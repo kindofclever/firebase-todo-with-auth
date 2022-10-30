a583bd3c0614ae925a4ce1a8b0a90528d8ab78e4
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-10-30 10:05:24 +0100

    fixed bug with mapping over todos

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <>
       {openModal ? <Modal setOpenModal={setOpenModal} /> : <></>}
-      <div className="sticky top-0 left-0 flex justify-between items-center p-4 md:p-5 bg-[#0d294b] border-b-2 md:border-b-4 border-solid border-[#fcffff]">
+      <div className="sticky top-0 left-0 flex justify-between items-center p-4 md:p-5 bg-[#2a3692] border-b-2 md:border-b-4 border-solid border-[#fcffff]">
         <h1 className="select-none uppercase">Todos</h1>
         <BsFillFilePersonFill
           onClick={() => setOpenModal(true)}
--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import TodoCard from "./TodoCard";
 
 const UserDashboard = () => {
   const [addTodo, setAddTodo] = useState(false);
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState({});
-  const { userInfo } = useAuth;
+  const { userInfo } = useAuth();
   console.log(todoList);
 
   useEffect(() => {
@@ -15,11 +16,13 @@ const UserDashboard = () => {
   }, [userInfo]);
 
   const handleAddTodo = async () => {
-    if (!todo) return;
+    if (!todo) {
+      return;
+    }
     const newKey =
       Object.keys(todoList).length === 0
         ? 1
-        : [Math.max(...Object.keys(todoList)) + 1];
+        : Math.max(...Object.keys(todoList)) + 1;
     setTodoList({
       ...todoList,
       [newKey]: todo
@@ -29,43 +32,45 @@ const UserDashboard = () => {
   return (
     <div className="w-full max-w-[65ch] mx-auto flex flex-col gap-4 md:gap-6 mt-5">
       {addTodo ? (
-        <div className="flex items-stretch">
+        <div className="flex items-stretch duration-300 border-b-2 md:border-b-4 border-solid border-[#fcffff] hover:border-[#ca1503]">
           <input
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
             type="text"
-            className="flex-1 outline-none p-2 duration-300 border-b-2 md:border-b-4 border-solid border-[#fcffff] focus:border-[#ca1503] bg-[#fcffff] placeholder-[#ca1503] placeholder-opacity-50"
+            className="flex-1 outline-none p-2  bg-[#fcffff] placeholder-[#ca1503] placeholder-opacity-50"
             placeholder="Enter a new todo here..."
           />
           <button
             onClick={handleAddTodo}
-            className="w-fit font-bold px-4 md:px-6 py-2 md:py-3 hover:text-[#fcffff] hover:bg-[#ca1503] duration-300 bg-[#fcffff] text-[#ca1503]">
+            className="w-fit font-bold px-4 md:px-6 py-2 md:py-3 hover:text-[#fcffff] hover:bg-[#ca1503] duration-300 bg-[#fcffff] text-[#ca1503] ">
             Add
           </button>
         </div>
       ) : (
         ""
       )}
-      {userInfo && (
+      {userInfo ? (
         <>
-          {Object.keys(
-            todoList.map((todo, index) => {
-              return (
-                <div className="text-[#fcffff]" key={index}>
-                  {todoList[todo]}
-                </div>
-              );
-            })
-          )}
+          {Object.keys(todoList).map((todo, index) => {
+            return <TodoCard key={index}>{todoList[todo]}</TodoCard>;
+          })}
         </>
+      ) : (
+        <></>
       )}
-      <button
-        onClick={() => setAddTodo(true)}
-        className="w-full p-2 bg-[#fcffff] text-[#ca1503] duration-300 relative
-        after:absolute after:top-0 after:right-full after:bg-[#ca1503] after:z-10 after:w-full after:h-full overflow-hidden 
-        hover:after:translate-x-full after:duration-300 hover:text-[#fcffff]">
-        <p className="relative z-20 font-bold text-center">Add a Todo</p>
-      </button>
+      {!addTodo ? (
+        <button
+          onClick={() => setAddTodo(true)}
+          className="w-full p-2 bg-[] text-[#fcffff] duration-300 relative
+        after:absolute after:top-0 after:right-full after:bg-[#fcffff] after:z-10 after:w-full after:h-full overflow-hidden 
+        hover:after:translate-x-full after:duration-300 hover:text-[#ca1503]">
+          <p className="relative z-20 font-bold text-center">
+            Start adding todos
+          </p>
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
